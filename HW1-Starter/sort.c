ca22cd18acd2688afa3eb581095b7d160a1484a0
Ryan Barron <barron62@purdue.edu>
2026-01-19 13:34:23 -0500

    Update sort.c

--- HW1-Starter/sort.c
+++ HW1-Starter/sort.c
@@ -5,4 +5,19 @@
 void ssort(int * arr, int size) {
 	/* For step 3, fill this in to perform a selection sort
 	   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
+	int min = arr[0];
+	int min_index = 0;
+	int temp;
+	for (int j = 1; j < size; j++){
+		min_index = j-1;
+		for (int i=j; i < size; i++){
+			if (arr[i] < arr[min_index]) {
+				min_index = i;
+			}
+		}
+		temp = arr[j-1];
+		arr[j-1] = arr[min_index];
+		arr[min_index = temp];
+		 
+	}
 }
