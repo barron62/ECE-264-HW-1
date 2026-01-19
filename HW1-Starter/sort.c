e2c380d2e7a694fed7c4df98ad783fbb5aaf86fb
barron62 <barron62@purdue.edu>
2026-01-19 14:50:14 -0500

    final

--- HW1-Starter/sort.c
+++ HW1-Starter/sort.c
@@ -5,19 +5,27 @@
 void ssort(int * arr, int size) {
 	/* For step 3, fill this in to perform a selection sort
 	   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
-	int min = arr[0];
-	int min_index = 0;
+	
+
+	//int min = arr[0];
+	int max_index = 0;
 	int temp;
 	for (int j = 1; j < size; j++){
-		min_index = j-1;
+		max_index = j-1;
 		for (int i=j; i < size; i++){
-			if (arr[i] < arr[min_index]) {
-				min_index = i;
-			}
+			#ifdef ASCENDING 
+				if (arr[i] < arr[max_index]) {
+					max_index = i;
+				}
+			#else
+				if (arr[i] > arr[max_index]) {
+					max_index = i;
+				}
+			#endif
 		}
 		temp = arr[j-1];
-		arr[j-1] = arr[min_index];
-		arr[min_index = temp];
+		arr[j-1] = arr[max_index];
+		arr[max_index] = temp;
 		 
 	}
 }
